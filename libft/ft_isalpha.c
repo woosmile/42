d2eb57968f1f93029afde8d5816d14c36fd5d23c
woosmile <wooseong1106@gmail.com>
2022-11-15 15:16:47 +0900

    cli test

--- libft/ft_isalpha.c
+++ libft/ft_isalpha.c
@@ -1,35 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: woosekim <woosekim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/14 15:38:32 by woosekim          #+#    #+#             */
-/*   Updated: 2022/11/15 15:16:28 by woosekim         ###   ########.fr       */
+/*   Created: 2022/11/15 15:16:03 by woosekim          #+#    #+#             */
+/*   Updated: 2022/11/15 15:16:32 by woosekim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(const char *s, int c)
-{
-	char	*s_temp;
-	char	c_temp;
-
-	s_temp = (char *)s;
-	c_temp = c;
-	if (c_temp == 0)
-	{
-		while (*s_temp != 0)
-			s_temp++;
-		return (s_temp);
-	}
-	while (*s_temp != 0)
-	{
-		if (*s_temp == c)
-			return (s_temp);
-		s_temp++;
-	}
-	return (0);
-}
